5c9c6dfa89347144aa626913dfeb9eda372d1020
vijay <vijaysalian94@gmail.com>
2023-06-07 22:53:46 +1000

    updated iFrame

---
+++ cypress/e2e/webdriver-uni/iFrame.cy.js
@@ -0,0 +1,26 @@
+
+
+describe("Handling iFrames & modal in webdriver uni page", ()=>{
+
+    it("Able to handle iFrame and modal in webdriver uni page", () =>{
+        cy.visit("http://www.webdriveruniversity.com")
+        cy.get("#iframe").invoke('removeAttr','target').click({force:true}) 
+        
+        cy.get('#frame').then($iframe=>{
+            const body = $iframe.contents().find('body')
+            cy.wrap(body).as('iFrame')
+        })
+       
+        cy.get('@iFrame').find('#button-find-out-more').click()
+        
+        cy.get('@iFrame').find('#myModal').as('modal')
+        cy.get('@modal').then($text=>{
+            const text = $text.text()
+            expect(text).to.include('Welcome to webdriveruniversity.com we sell a wide range of electrical goods')
+        })
+
+        cy.get('@modal').contains('Close').click()
+    })
+
+
+})
--- cypress/e2e/webdriver-uni/js-alerts.cy.js
+++ cypress/e2e/webdriver-uni/js-alerts.cy.js
@@ -1,53 +1,83 @@
-describe("Handling Js Alerts", ()=>{
+describe("Handling Js Alerts", () => {
 
-    it("Confirms Js alerts contains the correct texts", () =>{
-        cy.visit("http://www.webdriveruniversity.com")  
-        cy.get("#popup-alerts").invoke('removeAttr','target').click({force:true})
+    it("Confirms Js alerts contains the correct texts", () => {
+        cy.visit("http://www.webdriveruniversity.com")
+        cy.get("#popup-alerts").invoke('removeAttr', 'target').click({ force: true })
         cy.get('#button1').click()
 
-        cy.on('window:alert',(str) =>{
+        cy.on('window:alert', (str) => {
             expect(str).to.contain('I am an alert box!')
-            
+
         })
     })
 
-    it("Validate js confirm alert box works correctly when clicking OK", () =>{
-        cy.visit("http://www.webdriveruniversity.com")  
-        cy.get("#popup-alerts").invoke('removeAttr','target').click({force:true})
+    it("Validate js confirm alert box works correctly when clicking OK", () => {
+        cy.visit("http://www.webdriveruniversity.com")
+        cy.get("#popup-alerts").invoke('removeAttr', 'target').click({ force: true })
         cy.get('#button4').click()
 
-        cy.on('window:confirm',(str) =>{
+        cy.on('window:confirm', (str) => {
             expect(str).to.contain('Press a button!')
-            
+
         })
 
-        cy.get('#confirm-alert-text').should('have.text','You pressed OK!')
+        cy.get('#confirm-alert-text').should('have.text', 'You pressed OK!')
     })
 
-    it("Handling Modal popup", () =>{
-        cy.visit("http://www.webdriveruniversity.com")  
-        cy.get("#popup-alerts").invoke('removeAttr','target').click({force:true})
+    it("Handling Modal popup", () => {
+        cy.visit("http://www.webdriveruniversity.com")
+        cy.get("#popup-alerts").invoke('removeAttr', 'target').click({ force: true })
         cy.get('#button2').click()
 
-        cy.on('window:alert',(str) =>{
+        cy.on('window:alert', (str) => {
             expect(str).to.contain('It’s that Easy!! Well I think it is.....')
         })
         cy.get('.modal-footer > .btn').click()
     })
 
     //Challenge
-    it("Validate js confirm alert box works correctly when clicking CANCEL", () =>{
-        cy.visit("http://www.webdriveruniversity.com")  
-        cy.get("#popup-alerts").invoke('removeAttr','target').click({force:true})
+    it("Validate js confirm alert box works correctly when clicking CANCEL", () => {
+        cy.visit("http://www.webdriveruniversity.com")
+        cy.get("#popup-alerts").invoke('removeAttr', 'target').click({ force: true })
         cy.get('#button4').click()
 
-        cy.on('window:',(str) =>{
+        cy.on('window:', (str) => {
             expect(str).to.contain('Press a button!')
-            
+
         })
-        cy.on('window:confirm',() => false)
-        cy.get('#confirm-alert-text').should('have.text','You pressed Cancel!')
+        cy.on('window:confirm', () => false)
+        cy.get('#confirm-alert-text').should('have.text', 'You pressed Cancel!')
     })
 
+    it("Validate js confirm alert box works correctly using stub", () => {
+        cy.visit("http://www.webdriveruniversity.com");
+
+        cy.get("#popup-alerts").invoke('removeAttr', 'target').click({ force: true });
+
+        cy.window().then((win) => {
+            const stub = cy.stub(win, 'confirm').returns(false);
+
+            cy.get('#button4').click().then(() => {
+                expect(stub).to.be.calledWith('Press a button!');
+            });
+
+            cy.get('#confirm-alert-text').should('have.text', 'You pressed Cancel!');
+        });
+    })
+
+    it.only('should validate AJAX loader and alert box', () => {
+        cy.visit('http://www.webdriveruniversity.com');
+
+        cy.get('#popup-alerts').invoke('removeAttr', 'target').click({ force: true });
+        cy.get('#button3').click();  
+      
+        cy.intercept('GET', 'http://www.webdriveruniversity.com/Popup-Alerts/index.html').as('ajaxRequest'); // Replace 'your-ajax-url' with the actual AJAX URL
+        cy.wait('@ajaxRequest', { timeout: 5000 })
+      
+        cy.get('#button1').click();      
+        cy.on('window:alert', (message) => {
+          expect(message).to.contain('Alert message');
+        });
+      });      
 
 })
