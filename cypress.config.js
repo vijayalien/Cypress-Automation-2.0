ea8a915eb132deb062c841951828f27df6535c1a
vijay <vijaysalian94@gmail.com>
2023-08-03 22:05:29 +1000

    config file updates

--- cypress.config.js
+++ cypress.config.js
@@ -2,21 +2,29 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl:"http://www.webdriveruniversity.com/",
+    baseUrl: "http://www.webdriveruniversity.com/",
     chromeWebSecurity: false,
     defaultCommandTimeout: 3000,
-    pageLoadTimeout : 30000,
-    videoUploadOnPasses : false,
+    pageLoadTimeout: 30000,
+    videoUploadOnPasses: false,
     projectId: "7r1w25",
+    reporterEnabled: 'spec, mocha-junit-reporter',
+    mochaJunitReporterReporterOptions: {
+      mochaFile: 'cypress/results/junit/results-[hash].xml',
+    },
+    reporter: 'cypress-multi-reporters',
+    reporterOptions: {
+      configFile: 'reporter-config.json',
+    },
     // setupNodeEvents(on, config) {
     //   // implement node event listeners here
-      
+
     // },
   },
-  env:{
-    webdriver_uniurl:"http://www.webdriveruniversity.com",
+  env: {
+    webdriver_uniurl: "http://www.webdriveruniversity.com",
     first_name: "Cypress",
-    last_name:"Automation",
-    email:"cypressauto@test.com"
+    last_name: "Automation",
+    email: "cypressauto@test.com"
   }
 });
---
+++ cypress/e2e/automation-test-store/clearCookiesAndStorage.js
@@ -0,0 +1,31 @@
+///<reference types="Cypress" />
+
+import automationStore_HomePage_PO from "../../support/pageObjects/automation-test-store/automationStore_HomePage_PO"
+
+
+describe("Add multiple products to cart", ()=>{
+    const auomtationTestHomePage_PO = new automationStore_HomePage_PO()
+
+    before(()=>{
+        cy.fixture("products").then(function(data){
+            //this.data= data
+            globalThis.data=data
+        })
+    })
+
+    beforeEach(()=>{
+        cy.clearLocalStorage()
+        cy.clearCookies()
+        auomtationTestHomePage_PO.accessToUrl()
+        auomtationTestHomePage_PO.clickOnHairCare()
+    })
+
+    it("Adding specific items to basket", () =>{
+        globalThis.data.productName.forEach(function(element){
+            cy.addProductsToCart(element)
+        })
+      
+        cy.get('.dropdown-toggle > .fa').click()
+    })
+
+})
